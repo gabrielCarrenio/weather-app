d7148d7b51d9aa8cdf3ffbd90a9b4d18e3afa5dd
Gabriel Carreño <carrenocarlosgabriel@gmail.com>
2022-07-29 14:32:39 -0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Cards from "./components/Cards";
 import Nav from "./components/Nav";
+import axios from "axios";
 const isCityRepeat = (collection, city) => {
   const value = collection.find(({ name }) => name === city);
   return value === city;
@@ -10,30 +11,29 @@ function App() {
   const [cities, setCities] = useState([]);
 
   function onSearch(city) {
-    fetch(
+    axios(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${"f9f30e85a3620f9348c6224619794461"}&units=metric`
-    )
-      .then((r) => r.json())
-      .then((response) => {
-        if (response.main !== undefined) {
-          const city = {
-            min: Math.round(response.main.temp_min),
-            max: Math.round(response.main.temp_max),
-            img: response.weather[0].icon,
-            id: response.id,
-            wind: response.wind.speed,
-            temp: response.main.temp,
-            name: response.name,
-            weather: response.weather[0].main,
-            clouds: response.clouds.all,
-            latitud: response.coord.lat,
-            longitud: response.coord.lon,
-          };
-          setCities((oldCities) => [...oldCities, city]);
-        } else {
-          alert("City no found");
-        }
-      });
+    ).then(({ data }) => {
+      const { main } = data;
+      if (main !== undefined) {
+        const city = {
+          min: Math.round(main.temp_min),
+          max: Math.round(main.temp_max),
+          img: data.weather[0].icon,
+          id: data.id,
+          wind: data.wind.speed,
+          temp: data.main.temp,
+          name: data.name,
+          weather: data.weather[0].main,
+          clouds: data.clouds.all,
+          latitud: data.coord.lat,
+          longitud: data.coord.lon,
+        };
+        setCities((oldCities) => [...oldCities, city]);
+      } else {
+        alert("City no found");
+      }
+    });
   }
 
   return (
